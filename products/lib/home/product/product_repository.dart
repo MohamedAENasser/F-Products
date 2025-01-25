edc0a5f471fac2e9de69e0bf74a8d049c002043a
MohammedNasser12 <mohammed_abdelnasser@yahoo.com>
2025-01-25 02:49:53 +0200

    Add Product Repository

---
+++ products/lib/home/product/product_repository.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:products/home/product/product_item.dart';
+import 'package:http/http.dart' as http;
+
+class ProductRepository {
+  final String apiUrl = "https://fakestoreapi.com/products";
+
+  Future<List<ProductModel>> fetchProducts() async {
+    final response = await HTTP.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      return data.map((item) => ProductModel.fromJson(item)).toList();
+    } else {
+      throw Exception("Failed to load products");
+    }
+  }
+}
